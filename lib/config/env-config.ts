3e725e54b51ab748fa3cdd76f4404fc3fa1a9e74
thitiwat-t <85502719+thitiwat-t@users.noreply.github.com>
2023-02-25 00:50:21 +0700

    🔨build: add `env-config.ts`

---
+++ lib/config/env-config.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export default {
+  PORT: process.env.PORT ?? 3000,
+  DOMAIN: process.env.DOMAIN ?? 'http://localhost:3000',
+  JWT_SECRET: process.env.JWT_SECRET ?? '',
+  NODE_ENV: process.env.NODE_ENV ?? 'development',
+  get MONGODB_URI(): string | null {
+    return this.NODE_ENV === 'test' ? process.env.MONGODB_URI_TEST ?? null : process.env.MONGODB_URI ?? null
+  },
+} as const
