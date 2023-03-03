a7f9e73a804a3ae423a805466af7e6a928e5de8d
thitiwat-t <85502719+thitiwat-t@users.noreply.github.com>
2023-03-04 01:41:30 +0700

    🚨fix: misconfig middleware function chain

--- lib/app.ts
+++ lib/app.ts
@@ -26,7 +26,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(passport.initialize())
 
-app.use('/status', serverStatus(app), isLogin, isAdmin)
+app.use('/status', isLogin, isAdmin, serverStatus(app))
 app.use('/member', memberRouter)
 app.use('/product', productRouter)
 
